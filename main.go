7e0237c00194ed9e3a5a19fb3ee3709a1dc90b4b
yann <pizixi125@gmail.com>
2023-08-09 22:24:36 +0800

    优化

--- main.go
+++ main.go
@@ -1,56 +1,57 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"os/exec"
+    "fmt"
+    "io"
+    "net/http"
+    "os"
+    "os/exec"
 )
 
 const oneAPIURL = "https://github.com/songquanpeng/one-api/releases/download/v0.5.2/one-api"
+const oneAPIFileName = "one-api"
 
 func main() {
-
-	// 下载 one-api 文件
-	err := downloadFile("one-api", oneAPIURL)
-	if err != nil {
-		fmt.Println("Error downloading one-api:", err)
-		return
-	}
-
-	// 赋予权限
-	err = os.Chmod("one-api", 0755)
-	if err != nil {
-		fmt.Println("Error setting permissions for one-api:", err)
-		return
-	}
-
-	// 运行 one-api
-	cmd := exec.Command("./one-api")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err = cmd.Run()
-	if err != nil {
-		fmt.Println("Error running one-api:", err)
-		return
-	}
-
+    if err := runOneAPI(); err != nil {
+        fmt.Println("Error:", err)
+    }
 }
 
-func downloadFile(filepath string, url string) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
+func runOneAPI() error {
+    if err := downloadOneAPI(oneAPIFileName, oneAPIURL); err != nil {
+        return fmt.Errorf("download error: %w", err)
+    }
 
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
+    if err := os.Chmod(oneAPIFileName, 0755); err != nil {
+        return fmt.Errorf("permission error: %w", err)
+    }
 
-	_, err = io.Copy(out, resp.Body)
-	return err
+    cmd := exec.Command("./" + oneAPIFileName)
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    if err := cmd.Run(); err != nil {
+        return fmt.Errorf("execution error: %w", err)
+    }
+
+    return nil
+}
+
+func downloadOneAPI(filepath string, url string) error {
+    resp, err := http.Get(url)
+    if err != nil {
+        return fmt.Errorf("HTTP GET error: %w", err)
+    }
+    defer resp.Body.Close()
+
+    out, err := os.Create(filepath)
+    if err != nil {
+        return fmt.Errorf("file creation error: %w", err)
+    }
+    defer out.Close()
+
+    if _, err := io.Copy(out, resp.Body); err != nil {
+        return fmt.Errorf("file copy error: %w", err)
+    }
+
+    return nil
 }
